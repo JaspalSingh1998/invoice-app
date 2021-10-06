dadc1d80c19f1b6ec2994be7032ac5a7db923956
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-10-06 14:28:58 +0530

    Added Invoice List

--- src/componet/Invoice/Invoice.component.jsx
+++ src/componet/Invoice/Invoice.component.jsx
@@ -1,10 +1,11 @@
 import ArrowDown from "../../assets/icon-arrow-down.svg";
 import AddButton from "../../assets/icon-plus.svg";
+import ArrowRight from "../../assets/icon-arrow-right.svg";
 import "./Invoice.styles.scss";
 
 function Invoices() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <div className="invoice__header">
         <div className="header__title">
           <h2 className="title">Invoices</h2>
@@ -27,6 +28,83 @@ function Invoices() {
               <img src={AddButton} alt="Add New Invoice" />
             </div>
             <h3>New Invoice</h3>
+          </div>
+        </div>
+      </div>
+      <div className="invoices-list">
+        <div className="invoice">
+          <div className="invoice__id">
+            <h3>
+              <span>#</span>
+              RT3080
+            </h3>
+          </div>
+          <div className="date-wrapper">
+            <p className="date">Due 19 Aug 2021</p>
+          </div>
+          <div className="name-wrapper">
+            <h4 className="name">Jaspal Singh</h4>
+          </div>
+          <div className="price-wrapper">
+            <h2 className="price">£ 1,800.90</h2>
+          </div>
+          <div className="status-wrapper">
+            <div className="status paid">
+              <p>Paid</p>
+            </div>
+            <span>
+              <img src={ArrowRight} alt="View Invoice" className="view" />
+            </span>
+          </div>
+        </div>
+        <div className="invoice">
+          <div className="invoice__id">
+            <h3>
+              <span>#</span>
+              RT3080
+            </h3>
+          </div>
+          <div className="date-wrapper">
+            <p className="date">Due 19 Aug 2021</p>
+          </div>
+          <div className="name-wrapper">
+            <h4 className="name">Jaspal Singh</h4>
+          </div>
+          <div className="price-wrapper">
+            <h2 className="price">£ 1,800.90</h2>
+          </div>
+          <div className="status-wrapper">
+            <div className="status pending">
+              <p>Pending</p>
+            </div>
+            <span>
+              <img src={ArrowRight} alt="View Invoice" className="view" />
+            </span>
+          </div>
+        </div>
+        <div className="invoice">
+          <div className="invoice__id">
+            <h3>
+              <span>#</span>
+              RT3080
+            </h3>
+          </div>
+          <div className="date-wrapper">
+            <p className="date">Due 19 Aug 2021</p>
+          </div>
+          <div className="name-wrapper">
+            <h4 className="name">Jaspal Singh</h4>
+          </div>
+          <div className="price-wrapper">
+            <h2 className="price">£ 1,800.90</h2>
+          </div>
+          <div className="status-wrapper">
+            <div className="status draft">
+              <p>Draft</p>
+            </div>
+            <span>
+              <img src={ArrowRight} alt="View Invoice" className="view" />
+            </span>
           </div>
         </div>
       </div>
